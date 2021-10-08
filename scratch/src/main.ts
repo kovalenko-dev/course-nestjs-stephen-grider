d10932474ec92d04d3f1c6a72de28101a408399e
Alexey Kovalenko <a.kovalenko@nimbusplatform.io>
2021-10-09 01:51:21 +0300

    Добавил модуль и функцию для запуска приложения

--- scratch/src/main.ts
+++ scratch/src/main.ts
@@ -1,4 +1,5 @@
 import { Controller, Module, Get } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 
 @Controller()
 class AppController {
@@ -9,3 +10,16 @@ class AppController {
     }
 
 }
+
+@Module({
+    controllers: [AppController]
+})
+class AppModule {}
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+
+    await app.listen(3000);
+}
+
+bootstrap();
